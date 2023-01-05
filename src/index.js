d7e3915aa8ed14ae65e964d8346642183159b943
Janus Hasseriis <janus@hasseriis.net>
2023-01-05 21:53:18 +0100

    Triangles for intersections

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import "p5";
 // Use random functions from stdio and not from p5
 import { random, randomBoolean, weight } from "@altesc/stdio";
 
+const title = "Genuary 23 - 04";
+
 let c, w, h;
 let windowScale;
 let pd = 1;
@@ -12,9 +14,9 @@ const renderSize = 1000;
 const referenceSize = 1000;
 const aspect = 1 / 1;
 
-let noiseMax = 2;
-let slider;
-let zoff = 0;
+let triangles = 5;
+let triangleOffset = referenceSize / 10;
+let triangleSize = 4 * triangleOffset;
 
 window.setup = function () {
   //Math.random = fxrand();
@@ -25,59 +27,54 @@ window.setup = function () {
   pixelDensity(pd);
 
   c = createCanvas(w, h);
-  slider = createSlider(0, 10, 0, 0.1);
-  angleMode(DEGREES);
-  // colorMode(HSB);
+  // slider = createSlider(0, 10, 0, 0.1);
+  // angleMode(DEGREES);
+  colorMode(HSB, 360, 100, 100, 100);
 };
 
 window.draw = function () {
-  background(0);
-  translate(w / 2, h / 2);
-  //noiseMax = slider.value();
-  beginShape();
-  for (let a = 0; a < 360; a += 0.1) {
-    let xoff = map(cos(a), -1, 1, 0, noiseMax);
-    let yoff = map(sin(a), -1, 1, 0, noiseMax);
-    let r = map(noise(xoff, yoff, zoff), 0, 1, 150, 250);
-    let x = r * cos(a);
-    let y = r * sin(a);
-    stroke(255);
-    noFill();
-    vertex(x, y);
-    //t += 0.01;
+  background(30, 12, 93, 100);
+  noStroke();
+  let tw = triangleOffset * windowScale;
+  for (let row = 0; row < triangles; row++) {
+    fill(132, row * 12, 78, 35); // 132, 29, 78
+    triangle(
+      0,
+      row * tw + tw,
+      w,
+      row * tw + 2 * tw + tw,
+      0,
+      row * tw + 4 * tw + tw
+    );
+    fill(321, row * 12, 76, 35); // 321, 53, 76
+    triangle(
+      w,
+      row * tw + tw,
+      0,
+      row * tw + 2 * tw + tw,
+      w,
+      row * tw + 4 * tw + tw
+    );
   }
-  endShape(CLOSE);
 
-  for (let a = 0; a < 360; a += 90) {
-    background(0);
-    let xoff = map(cos(a), -1, 1, 0, noiseMax);
-    let yoff = map(sin(a), -1, 1, 0, noiseMax);
-    stroke(255);
-
-    // fill(255);
-    // noStroke();
-    let arcAngle = map(noise(xoff, yoff), 0, 1, 0, 360);
-    // console.log("ANGLE: ", arcAngle);
-    arc(0, 0, 250, 250, 0 + arcAngle, 90 + arcAngle);
-    //t += 0.01;
-  }
-  zoff += 0.01;
-
-  /*   strokeWeight(3);
-  stroke(100);
-  strokeWeight(2);
-  line(0, h / 2, w, h / 2);
-  line(w / 2, 0, w / 2, h);
-  line(margin, 0, margin, h);
-  line(w - margin, 0, w - margin, h);
-  line(0, margin, w, margin);
-  line(0, h - margin, w, h - margin); */
+  // strokeWeight(3);
+  // stroke(100);
+  // strokeWeight(2);
+  // line(0, h / 2, w, h / 2);
+  // line(w / 2, 0, w / 2, h);
+  // line(0, boxSize, w, boxSize);
+  // line(boxSize, 0, boxSize, h);
+  // line(margin, 0, margin, h);
+  // line(w - margin, 0, w - margin, h);
+  // line(0, margin, w, margin);
+  // line(0, h - margin, w, h - margin);
 
   //fxpreview();
+  noLoop();
 };
 
 features = {
-  Genuary: "01",
+  Title: title,
 };
 
 console.table(features);
@@ -120,7 +117,7 @@ function setDimensions() {
 window.keyPressed = function () {
   if (key === "s" || key === "S") {
     console.log("SAVE");
-    saveCanvas("Genuary 23 - 01" + width * pd + "x" + height * pd, "png");
+    saveCanvas(title + width * pd + "x" + height * pd, "png");
   }
   if (
     key === "1" ||
