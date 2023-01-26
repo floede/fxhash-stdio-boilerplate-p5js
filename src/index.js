587cb513548ee26e902caa828ad15751f0b2c359
Janus Hasseriis <janus@hasseriis.net>
2023-01-26 22:01:03 +0100

    Add SDF

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import "p5";
 // Use random functions from stdio and not from p5
 import { random, randomBoolean, weight } from "@altesc/stdio";
 
-const title = "Genuary 23 - 24";
+const title = "Genuary 23 - 25";
 
 let canvas, w, h;
 let windowScale;
@@ -17,7 +17,7 @@ const yellow = [55, 95, 98]; // #f9e60d
 const black = [216, 33, 34]; // #3a4556
 
 var n = 0;
-var c = 7;
+var c = 5;
 
 var points = [];
 
@@ -52,14 +52,16 @@ window.draw = function () {
     var y = r * sin(a);
     var hu = sin(start + i * 0.1);
     hu = map(hu, -1, 1, 0, 360);
+    //noFill();
     fill(black[0], black[1], black[2] + random(-3, 3));
-    //noStroke();
-    stroke(yellow);
-    if (k(abs(x) - margin, abs(y) - margin) < 0) {
-      ellipse(x, y, c + (0.2 * i) / 40, c + (0.2 * i) / 40);
+    noStroke();
+    //stroke(black);
+    //strokeWeight(c / 4);
+    if (k(abs(x) - 300, abs(y) - 300) < 0) {
+      ellipse(x, y, c + (0.08 * i) / 40, c + (0.08 * i) / 40);
     }
   }
-  n += 5;
+  n += 6;
   start += 0.1;
 
   // strokeWeight(2);
@@ -73,7 +75,7 @@ window.draw = function () {
   // line(0, h - tw, w, h - tw);
 
   //fxpreview();
-  if (n > 6000) {
+  if (n > 10000) {
     noLoop();
   }
   // noLoop();
@@ -90,6 +92,9 @@ function windowResized() {
   setDimensions();
   resizeCanvas(w, h);
 }
+
+let R = (a = 1) => Math.random() * a;
+let L = (x, y) => (x * x + y * y) ** 0.5;
 
 let k = (a, b) => (a > 0 && b > 0 ? L(a, b) : a > b ? a : b);
 
